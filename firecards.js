ee2ed35cf0b4bf54450ad92c0d6514b0a7d51728
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-07-19 21:58:31 +0200

    Lots of stuff

--- firecards.js
+++ firecards.js
@@ -1,13 +1,76 @@
-angular.module('fireCardsApp', [])
-    .controller('FireCardsController', function($scope) {
-        $scope.newContacts = [
-            {
-                fullName: "Antoine d'Otreppe",
-                phoneNumber: "+32478625648",
-                selected: true
-            }
-        ];
+angular.module('fireCardsApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'LoginController',
+                templateUrl: 'login.html'
+            })
+            .when('/contactList', {
+                controller: 'ContactListController',
+                templateUrl: 'contactList.html'
+            })
+    })
+    .controller('LoginController', function ($scope, $location, cardDav) {
+        $scope.connect = function () {
+            cardDav.connect(
+                $scope.serverAddress,
+                $scope.username,
+                $scope.password
+            );
 
-        $scope.updatedContacts = $scope.newContacts;
+            $location.path("/contactList");
+            // TODO Go to ContactListController
+        }
+    })
+    .controller('ContactListController', function($scope, cardDav) {
+        var allContacts = cardDav.listAllContacts();
+
+        $scope.contactList = allContacts;
+
+        $scope.clearSearch = function () {
+            console.info("Clear search terms");
+
+            $scope.searchTerms = "";
+        };
+
+        $scope.filterList = function () {
+            console.info("Filter contact list");
+
+            var searchTerms = $scope.searchTerms.toLowerCase();
+
+            if ($scope.searchTerms == "") {
+                $scope.contactList = allContacts;
+            } else {
+                $scope.contactList = allContacts.filter(function (contact) {
+                    var lowerName = contact.fullName.toLowerCase()
+                    return lowerName.indexOf(searchTerms) !== -1;
+                });
+            }
+        };
+
+        $scope.selectContact = function (contact) {
+            console.info("Select contact");
+        };
+    })
+    .factory('cardDav', function () {
+        return {
+            connect: function (serverAddr, username, password) {
+                console.info("Connecting to " + serverAddr);
+            },
+            listAllContacts: function () {
+                return [
+                    {
+                        fullName: "Fox Thered",
+                        phoneNumber: "+4487967578",
+                        selected: true
+                    },
+                    {
+                        fullName: "Antoine d'Otreppe",
+                        phoneNumber: "+32478625648",
+                        selected: true
+                    }
+                ];
+            }
+        };
     });
 
